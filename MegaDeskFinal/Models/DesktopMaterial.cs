b562ab3c0a5cb019e54df8bdd2510435c0a0404d
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-11-04 13:26:35 -0600

    adding classes

---
+++ MegaDeskFinal/Models/DesktopMaterial.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.IO;
+
+namespace RazorPagesMovie.Models
+{
+        public enum DesktopMaterial
+    {
+        Laminate,
+        Oak,
+        Rosewood,
+        Veneer,
+        Pine
+    }
+}
