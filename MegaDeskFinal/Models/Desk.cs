3059e636d8a1ba170519eabeb07d14463cdf825a
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-11-03 18:30:25 -0600

    ...

--- MegaDeskFinal/Models/Desk.cs
+++ MegaDeskFinal/Models/Desk.cs
@@ -5,10 +5,30 @@ namespace RazorPagesMovie.Models
 {
     public class Desk
     {
+        public const short MIN_WIDTH = 24;
+        public const short MAX_WIDTH = 96;
+        public const short MIN_DEPTH = 12;
+        public const short MAX_DEPTH = 48;
+        public const short MIN_DESK_DRAWERS = 0;
+        public const short MAX_DESK_DRAWERS = 7;
+
         public int ID { get; set; }
+        public decimal Width { get; set; }
+        public decimal Depth { get; set; }
+         public int NumberOfDrawers { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public decimal Price { get; set; }
+        public DesktopMaterial desktopMaterial { get; set; }
+    }
+
+    public enum DesktopMaterial
+    {
+        Laminate,
+        Oak,
+        Rosewood,
+        Veneer,
+        Pine
     }
 }
